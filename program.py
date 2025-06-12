53cedc75cb6a7ba794cfd9061d2bfb5bab509b6d
Agabaruhanga Malcolm <malcolmagabaruhanga@gmail.com>
2025-06-12 11:26:32 +0300

    Add files via upload

---
+++ program.py
@@ -0,0 +1,25 @@
+print("grading scale based on students marks")
+marks=int(input("please enter marks between 0 and 100"))
+if marks >=91 and marks <=100:
+    print("A+")
+elif marks >=81 and marks <=90:
+    print("A") 
+elif marks >=71 and marks <=80:
+    print("B+")
+elif marks >=61 and marks <=70:
+    print("B")
+elif marks >=51 and marks <=60:
+    print("C+")
+elif marks >=41 and marks <=50:
+    print("C") 
+elif marks >=31 and marks <=40:
+    print("D")
+elif marks >=21 and marks <=30:
+    print("E")
+elif marks >=0 and marks <=20:
+    print("F")
+else:
+    print("invalid please enter marks between 0 and 100")    
+
+
+          
---
+++ variables.py
@@ -0,0 +1,19 @@
+""" Declaring a variable means to inform the interpreter about the existence of  variable, its name and data type while
+assigning a variable means to store a specific piece of data into the memory location that has been allocated for that
+variable. """
+
+
+
+#integer "8"
+#convert the integer to a string
+string_int = "8"
+string_int = int(string_int)
+print(string_int)
+
+length_string ="56.90"
+length_int =round(float(length_string))
+print(length_int)
+
+height_string = "34.09944"
+height_integer =round(float(height_string))
+print(height_integer)
